5b9788a5e91a69fb69f804f1a5817cb40830cc45
KristinaProject <kristtule@gmail.com>
2023-03-06 20:37:59 +0200

    some header changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,21 @@
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+
 export function Header() {
   return (
     <div id="header">
       <p>Dogs breed</p>
+      <GiHamburgerMenu />
       <ul>
-        <li>Home</li>
-        <li>Find breed</li>
-        <li>Contact</li>
+        <li>
+          <Link>Home</Link>
+        </li>
+        <li>
+          <Link> Find breed</Link>
+        </li>
+        <li>
+          <Link> Contact</Link>
+        </li>
       </ul>
     </div>
   );
