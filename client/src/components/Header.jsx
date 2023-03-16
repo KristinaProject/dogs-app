c4e2d5473bdde64121d34d5fffc2b0f9d7be2a29
KristinaProject <kristtule@gmail.com>
2023-03-16 09:48:37 +0200

    feat: responsive menu button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,44 @@
 import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useEffect, useState } from "react";
 
 export function Header() {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    function updateDimension() {
+      const widthInner = window.innerWidth;
+      setWidth(widthInner);
+    }
+
+    updateDimension();
+    window.addEventListener("resize", updateDimension);
+
+    return () => {
+      window.removeEventListener("resize", updateDimension);
+    };
+  }, []);
+
   return (
     <div id="header">
       <p>Dogs breed</p>
-      <GiHamburgerMenu />
-      <ul>
-        <li>
-          <Link>Home</Link>
-        </li>
-        <li>
-          <Link> Find breed</Link>
-        </li>
-        <li>
-          <Link> Contact</Link>
-        </li>
-      </ul>
+      {width < 700 ? (
+        <button>
+          <GiHamburgerMenu />
+        </button>
+      ) : (
+        <ul>
+          <li>
+            <Link>Home</Link>
+          </li>
+          <li>
+            <Link> Find breed</Link>
+          </li>
+          <li>
+            <Link> Contact</Link>
+          </li>
+        </ul>
+      )}
     </div>
   );
 }
