8e866d74b624233db86bb65a060ae0c845652f06
KristinaProject <kristtule@gmail.com>
2023-04-19 11:15:20 +0300

    feat: random image in cards

--- client/src/components/CardList.jsx
+++ client/src/components/CardList.jsx
@@ -1,18 +1,39 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+function Card({ name, image }) {
+  return (
+    <div>
+      <div className="dog-image">
+        <img src={image} alt={image} />
+      </div>
+      <span>{name}</span>
+    </div>
+  );
+}
+
 export function CardList(props) {
+  const [info, setInfo] = useState(null);
+  useEffect(() => {
+    async function fetchingData() {
+      const response = await fetch("http://localhost:8880/random");
+      const data = await response.json();
+      return setInfo(data.message);
+    }
+
+    fetchingData();
+  }, []);
+
   return (
     <ul className={props.class} id="cardlist">
       <li className="card">
         <Link>
-          <div className="dog-image"></div>
-          <span>All breeds</span>
+          <Card image={info} name="Random breeds" />
         </Link>
       </li>
       <li className="card">
         <Link>
-          <div className="dog-image"></div>
-          <span>Random breeds</span>
+          <Card image={info} name="Breeds by category" />
         </Link>
       </li>
     </ul>
--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import express from "express";
 const dogs = "https://dog.ceo/api/breeds/list/all";
 const app = express();
 import fetch from "node-fetch";
+import cors from "cors";
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/random", async (req, res) => {
   try {
-    const response = await fetch("https://dog.ceo/api/breeds/list/all");
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
     const data = await response.json();
     res.json(data);
   } catch (error) {
